81c4329c26180454501a79b031a73db275f86dba
paraschivuRazvan <razvan.mirel@yahoo.com>
2021-09-06 21:38:57 +0200

    Add initial component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CardComponent } from './components/card/card.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CardComponent,
+    DetailsPageComponent
   ],
   imports: [
     BrowserModule,
